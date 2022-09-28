9392f133e9a08a705af577e3e323ec4555356045
Reymon Reyes <elmoe01@gmail.com>
2022-09-28 10:41:47 +0800

    Part 4 Demo

---
+++ TaskManagerApp/Data/TaskManagerAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models = TaskManagerApp.Models;
+
+namespace TaskManagerApp.Data
+{
+    public class TaskManagerAppContext : DbContext
+    {
+        public DbSet<Models.Task> Tasks { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=REYMON\\SQLEXPRESS;Initial Catalog=TaskManagerApp;User ID=appuser;Password=mypassword;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ TaskManagerApp/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Principal;
+
+namespace TaskManagerApp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
